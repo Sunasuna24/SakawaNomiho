06b99180c013b20f0ccf7e2b4d9863a832646b41
Sunasuna24 <sunasunayaka1218@gmail.com>
2023-09-23 18:09:23 +0900

    ログイン画面を表示した

---
+++ resources/views/login.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログイン | 酒笑</title>
+</head>
+<body>
+    <main>
+        <p>ログインする</p>
+    </main>
+</body>
+</html>
